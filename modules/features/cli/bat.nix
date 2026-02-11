2402d76a62084a1d7802f4be2c5874f304555170
KodNova <Dev@KodNova.dev>
2026-02-11 02:36:26 -0600

    add: bat

---
+++ modules/features/cli/bat.nix
@@ -0,0 +1,23 @@
+_: {
+  flake.nixosModules.base = {pkgs, ...}: {
+    my.home = {
+      programs.bat = {
+        enable = true;
+        config = {
+          theme = "Catppuccin Mocha";
+        };
+        themes = {
+          "Catppuccin Mocha" = {
+            src = pkgs.fetchFromGitHub {
+              owner = "catppuccin";
+              repo = "bat";
+              rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+              sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+            };
+            file = "themes/Catppuccin Mocha.tmTheme";
+          };
+        };
+      };
+    };
+  };
+}
