63b244449ae43e918b668abac0f8d0918875281a
KodNova <Dev@KodNova.dev>
2026-02-11 03:31:38 -0600

    refactor: zsh.nix

--- modules/features/cli/zsh.nix
+++ modules/features/cli/zsh.nix
@@ -1,48 +1,66 @@
 _: {
-  flake.nixosModules.base = _: {
-    programs.zsh.enable = true;
+  flake.nixosModules = {
+    base = _: {
+      programs.zsh.enable = true;
 
-    my.home = {
-      programs.zsh = {
-        enable = true;
-        autocd = true;
-        autosuggestion.enable = false;
-        enableCompletion = true;
-        syntaxHighlighting.enable = true;
+      my.home = {
+        programs = {
+          zsh = {
+            enable = true;
+            autocd = true;
+            autosuggestion.enable = false;
+            enableCompletion = true;
+            syntaxHighlighting.enable = true;
 
-        shellAliases = {
-          ls = "eza --icons --group-directories-first";
-          ll = "eza -l --icons --git --group-directories-first";
-          la = "eza -la --icons --git --group-directories-first";
-          cat = "bat";
-          du = "dust";
-          ps = "procs";
-          grep = "rg";
-          find = "fd";
+            shellAliases = {
+              ls = "eza --icons --group-directories-first";
+              ll = "eza -l --icons --git --group-directories-first";
+              la = "eza -la --icons --git --group-directories-first";
+              cat = "bat";
+              du = "dust";
+              ps = "procs";
+              grep = "rg";
+              find = "fd";
 
-          sudo = "sudo ";
-          fastfetch = "fastfetch -l NixOS_old";
-          vim = "nix run github:KodNova/NeovimConfig-nfv";
+              sudo = "sudo ";
+              fastfetch = "fastfetch -l NixOS_old";
+              vim = "nix run github:KodNova/NeovimConfig-nfv";
+            };
+
+            oh-my-zsh = {
+              enable = true;
+              plugins = ["git"];
+            };
+          };
+
+          fzf = {
+            enable = true;
+            enableZshIntegration = true;
+          };
         };
-
-        oh-my-zsh = {
-          enable = true;
-          plugins = ["git"];
-        };
-      };
-
-      fzf = {
-        enable = true;
-        enableZshIntegration = true;
       };
     };
-  };
-  flake.nixosModules.solModule = _: {
-    my.home = {
-      programs.zsh = {
-        shellAliases = {
+
+    solModule = _: {
+      my.home = {
+        programs.zsh.shellAliases = {
           zpm = "ssh 100.72.122.3";
           puddlejumper = "ssh 100.78.244.126";
+        };
+      };
+    };
+
+    workstation = _: {
+      my.home = {
+        programs.zsh.shellAliases = {
+          odin = "/repos/odin/";
+          repo = "/repos";
+          H = "start-hyprland";
+
+          xavier = "ssh bobj@100.95.133.66";
+
+          olddev = "nix develop --command zsh";
+          dev = "devbox shell";
         };
       };
     };
