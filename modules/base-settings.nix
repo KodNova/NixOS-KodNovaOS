d111675453492b8b59508c3bdccd3489c73a62b5
kodnova <Dev@KodNova.dev>
2026-02-16 01:41:25 +0000

    add: timezone

--- modules/base-settings.nix
+++ modules/base-settings.nix
@@ -1,6 +1,7 @@
 _: {
   flake.nixosModules.base = _: {
     nixpkgs.config.allowUnfree = true;
+    time.timeZone = "Canada/Central";
 
     nix.settings = {
       experimental-features = ["nix-command" "flakes"];
