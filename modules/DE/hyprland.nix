ef6f4b97fa584e611cae3f4888a5f8efcc147e89
kodnova <Dev@KodNova.dev>
2026-02-27 14:34:08 -0600

    fix: portal for flatpak

--- modules/DE/hyprland.nix
+++ modules/DE/hyprland.nix
@@ -21,7 +21,15 @@ _: {
         pkgs.wl-clipboard
       ];
 
+      environment.sessionVariables = {
+        NIXOS_OZONE_WL = "1";
+        XDG_CURRENT_DESKTOP = "Hyprland";
+        XDG_SESSION_TYPE = "wayland";
+        XDG_SESSION_DESKTOP = "Hyprland";
+      };
+
       my.home = _: {
+        home.packages = [pkgs.xdg-desktop-portal-gtk]; # had to add this for flatpak plex to open browser.
         services.hypridle = {
           enable = true;
           settings = {
