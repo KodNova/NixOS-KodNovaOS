1b17469ac6f340c862b8632502b80a20c609ad55
kodnova <Dev@KodNova.dev>
2026-05-14 20:11:54 -0500

    add: docker to sol for dev work

--- modules/features/cli/docker.nix
+++ modules/features/cli/docker.nix
@@ -13,4 +13,19 @@ _: {
       programs.lazydocker.enable = true;
     };
   };
+
+  flake.nixosModules.solModule = {
+    pkgs,
+    config,
+    ...
+  }: {
+    virtualisation.docker.enable = true;
+
+    users.users.${config.my.username}.extraGroups = ["docker"];
+    environment.systemPackages = [pkgs.docker-compose];
+
+    my.home = {
+      programs.lazydocker.enable = true;
+    };
+  };
 }
